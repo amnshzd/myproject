a8e62c1395ceaa594fce1a0d25c817d8438859ef
Amin Shirzad <mr.aminshirzad@gmail.com>
2017-10-22 22:27:11 +0330

    first commit

---
+++ resources/views/show.blade.php
@@ -0,0 +1,8 @@
+
+@extends('welcome')
+@section('content')
+    <div style="text-align: center; font-weight: bold; font-size: 30px;background-color: gold; height: 230px; padding-top: 230px;">
+        Content In About Us Page
+    </div>
+@stop
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hitos:Laravel</title>
+</head>
+<body>
+<div style="text-align: center; font-weight: bold; font-size: 30px; background-color: green;">
+    Header In My Site
+</div>
+@yield('content')
+<div style="text-align: center; font-weight: bold; font-size: 30px;background-color: gray;">
+    Footer In My Site
+</div>
+</body>
+</html>
+
+Copy
